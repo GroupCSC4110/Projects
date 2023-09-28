8d5d8680c717fbb8e92f2b18f7ab7cd6ad431b3c
IssaPortfolio <Habeeb.H.Issa@gmail.com>
2023-09-27 20:44:57 -0400

    New Code

--- GroupProject1/Code/csv_gui.py
+++ GroupProject1/Code/csv_gui.py
@@ -1,93 +1,93 @@
 """
-Import, append, reverse, and query data from/to CSV Files.
+Import file, print data, append data, query data, and reverse data
 """
 import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox  # Import the messagebox module
 import csv
 
+# Global variable to store the file path
+FILE_PATH = None
+
 # Import data function
-
-
-def import_csv_file(file_path):
+def import_csv_file():
     """
-    :param param1: File path of CSV File
-    :type param1: String
-    "return: List of data
+    param: None
+    typeParam: None
+    rReturn: List of data
     rtype: List
     """
     data = []
-
-    # Open file to read
+    if not FILE_PATH:
+        messagebox.showerror("Error", "No file has been imported!")  # Show error
+        return []
     try:
-        with open(file_path, 'r', newline='', encoding='utf-8') as csvfile:
+        with open(FILE_PATH, 'r', newline='', encoding='utf-8') as csvfile:
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
                 data.append(row)
 
-    # Error handling
     except FileNotFoundError:
-        print(f"The file '{file_path}' was not found.")
+        messagebox.showerror("Error", f"The file '{FILE_PATH}' was not found!")  # Show error
     except FileExistsError:
-        print(f"{file_path} does not exist.")
+        messagebox.showerror("Error", f"{FILE_PATH} does not exist!")  # Show error
 
     return data
 
 # Append to file function
-
-
-def append_csv_file(file_path, data_list):
+def append_csv_file(data_list):
     """
-    :param param1: File path of CSV File
-    :param param2: Data List of data to append to file
-    :type param1: String
-    :type param2: List
-    "return: Nothing
-    rtype: Nothing
+    param: None
+    typeParam: None
+    rReturn: None
+    rtype: None
     """
 
-    # Open file to append
+    if not FILE_PATH:
+        messagebox.showerror("Error", "No file has been imported!")  # Show error
+        return
     try:
-        with open(file_path, 'a', newline='', encoding='utf-8') as csvfile:
+        with open(FILE_PATH, 'a', newline='', encoding='utf-8') as csvfile:
             csv_writer = csv.writer(csvfile)
             for line in data_list:
-                # Split each line by comma to create a list
                 row = line.split(",")
                 csv_writer.writerow(row)
 
-    # Error handling
     except FileNotFoundError:
-        print(f"The file '{file_path}' was not found.")
+        messagebox.showerror("Error", f"The file '{FILE_PATH}' was not found!")  # Show error
     except FileExistsError:
-        print(f"{file_path} does not exist.")
+        messagebox.showerror("Error", f"{FILE_PATH} does not exist!")  # Show error
 
 # Search functions
-
-
-def query_csv_file(file_path):
+def query_csv_file():
     """
-    :param param1: File path of CSV File
-    :type param1: String
-    "return: List of data
+    param: None
+    typeParam: None
+    rReturn: List of data
     rtype: List
     """
+
+    if not FILE_PATH:
+        messagebox.showerror("Error", "No file has been imported!")  # Show error
+        return []
     data = []
     text_widget_query2.config(state=tk.DISABLED)
     data_input = text_widget_query.get()
-
-    # Open file to read and gather data
     try:
-        with open(file_path, 'r', newline='', encoding='utf-8') as csvfile:
+        with open(FILE_PATH, 'r', newline='', encoding='utf-8') as csvfile:
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
-                if row[0] == data_input:
+                if row and row[0] == data_input:
                     data.append(row)
 
-    # Error handling
     except FileNotFoundError:
-        print(f"The file '{file_path}' was not found.")
+        messagebox.showerror("Error", f"The file '{FILE_PATH}' was not found!")  # Show error
+        return []
     except FileExistsError:
-        print(f"{file_path} does not exist.")
-    return data
+        messagebox.showerror("Error", f"{FILE_PATH} does not exist!")  # Show error
+        return []
 
+    return data
 
 custom_font = ("Times New Roman", 12)
 app = tk.Tk()
@@ -118,59 +118,52 @@ text_widget_query2 = tk.Text(app, wrap=tk.WORD, width=52,
                              height=10, bg="slategray2")
 
 # Import button configuration
-
-
 def import_button_click():
     """
-    :param param1: None
-    :type param1: None
-    "return: None
+    param: None
+    typeParam: None
+    rReturn: None
     rtype: None
     """
+    global FILE_PATH
     search.place_forget()
     text_widget_query.place_forget()
     text_widget_query2.place_forget()
     submit.place_forget()
     label.config(text="Data Imported!")
     label.place(x=300, y=0)
-    file_path = "order-details.csv"
-    csv_data = import_csv_file(file_path)
-    text_widget.config(state=tk.DISABLED)
-    text_widget.config(state=tk.NORMAL)
-    text_widget.delete("1.0", tk.END)
 
-    # Display the CSV data in the Text widget
-    for row in csv_data:
-        text_widget.insert(tk.END, ", ".join(row) + "\n")
+    # Ask the user to select a file
+    FILE_PATH = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
 
-    # Disable the Text widget after displaying the data
-    text_widget.config(state=tk.DISABLED)
-    text_widget.place(x=125, y=350)
+    if not FILE_PATH:
+        label.config(text="No file has been imported!")
+        return
 
 # Search button configuration
-
-
 def search_click():
     """
-    :param param1: None
-    :type param1: None
-    "return: None
+    param: None
+    typeParam: None
+    rReturn: None
     rtype: None
     """
+
+    if not FILE_PATH:
+        messagebox.showerror("Error", "No file has been imported!")  # Show error
+        return
     label.config(text="Data Queried!")
     label.place_configure(x=300, y=0)
-    data = query_csv_file(file_path="order-details.csv")
+    data = query_csv_file()
     not_found = len(data) == 0
     text_widget_query2.config(state=tk.NORMAL)
     text_widget_query2.delete("1.0", tk.END)
 
-    # Add data from list to text box
     for string in data:
         text_widget_query2.config(state=tk.NORMAL)
         text_widget_query2.insert(tk.END, ", ".join(string) + "\n")
         text_widget_query2.config(state=tk.DISABLED)
 
-    # If nothing found, print 'Not Found'
     if not_found:
         text_widget_query2.config(state=tk.NORMAL)
         text_widget_query2.delete("1.0", tk.END)
@@ -179,37 +172,42 @@ def search_click():
     text_widget_query.delete(0, "end")
 
 # Submit button configuration for appending
-
-
 def submit_click():
     """
-    :param param1: None
-    :type param1: None
-    "return: None
+    param: None
+    typeParam: None
+    rReturn: None
     rtype: None
     """
+
+    if not FILE_PATH:
+        messagebox.showerror("Error", "No file has been imported!")  # Show error
+        return
     label.place(x=300, y=0)
-    # Get the text from the Text widget
     user_input = text_widget_append.get("1.0", tk.END)
     input_list = [line.strip()
                   for line in user_input.split("\n") if line.strip()]
 
     if input_list:
-        append_csv_file("order-details.csv", input_list)
-        # Clear the Text widget after appending data
+        append_csv_file(input_list)
         text_widget_append.delete("1.0", tk.END)
         label.config(text="Data Added!")
 
 # Append button configuration
-
-
 def add_button_click():
     """
-    :param param1: None
-    :type param1: None
-    "return: None
+    param: None
+    typeParam: None
+    rReturn: None
     rtype: None
     """
+
+    if not FILE_PATH:
+        search.place_forget()
+        text_widget_query2.place_forget()
+        text_widget_query.place_forget()
+        messagebox.showerror("Error", "No file has been imported!")  # Show error
+        return
     search.place_forget()
     text_widget_query2.place_forget()
     text_widget_query.place_forget()
@@ -223,15 +221,23 @@ def add_button_click():
     submit.config(command=submit_click)
 
 # Query button configuration
-
-
 def query_button_click():
     """
-    :param param1: None
-    :type param1: None
-    "return: None
+    param: None
+    typeParam: None
+    rReturn: None
     rtype: None
     """
+
+    if not FILE_PATH:
+        submit.place_forget()
+        text_widget_query2.config(state=tk.NORMAL)
+        text_widget_query2.delete("1.0", tk.END)
+        text_widget_query2.config(state=tk.DISABLED)
+        text_widget.place_forget()
+        text_widget_append.place_forget()
+        messagebox.showerror("Error", "No file has been imported!")  # Show error
+        return
     submit.place_forget()
     text_widget_query2.config(state=tk.NORMAL)
     text_widget_query2.delete("1.0", tk.END)
@@ -248,15 +254,17 @@ def query_button_click():
     search.config(command=search_click)
 
 # Reorganize button configuration
-
-
 def reorganize_button_click():
     """
-    :param param1: None
-    :type param1: None
-    "return: None
+    param: None
+    typeParam: None
+    rReturn: None
     rtype: None
     """
+
+    if not FILE_PATH:
+        messagebox.showerror("Error", "No file has been imported!")  # Show error
+        return
     label.config(text="Data Re-Organized!")
     label.place(x=300, y=0)
     text_widget.place_forget()
@@ -266,27 +274,26 @@ def reorganize_button_click():
     text_widget_query.place_forget()
     text_widget_query2.place_forget()
     label.place(x=300, y=0)
-    csv_file_path = 'order-details.csv'
-
-    # Read the CSV file into a list of lists
-    with open(csv_file_path, mode='r', newline='', encoding="utf-8") as file:
-        csv_reader = csv.reader(file)
-        headers = next(csv_reader)  # Skip the headers
-        data = list(csv_reader)
-
-    # Sort the data based on the values in the first column (assuming they are strings)
+    try:
+        with open(FILE_PATH, mode='r', newline='', encoding="utf-8") as file:
+            csv_reader = csv.reader(file)
+            headers = next(csv_reader)
+            data = list(csv_reader)
+    except FileNotFoundError:
+        messagebox.showerror("Error", f"The file '{FILE_PATH}' was not found!")  # Show error
+    except FileExistsError:
+        messagebox.showerror("Error", f"{FILE_PATH} does not exist!")  # Show error
     data.sort(key=lambda row: row[0])
 
-    # Overwrite the original CSV file with the sorted data, including headers
-    with open(csv_file_path, mode='w', newline='', encoding="utf-8") as file:
-        csv_writer = csv.writer(file)
-
-        # Write the headers
-        csv_writer.writerow(headers)
-
-        # Write the sorted data
-        csv_writer.writerows(data)
-
+    try:
+        with open(FILE_PATH, mode='w', newline='', encoding="utf-8") as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(headers)
+            csv_writer.writerows(data)
+    except FileNotFoundError:
+        messagebox.showerror("Error", f"The file '{FILE_PATH}' was not found!")  # Show error
+    except FileExistsError:
+        messagebox.showerror("Error", f"{FILE_PATH} does not exist!")  # Show error
 
 import_button.config(command=import_button_click)
 add_button.config(command=add_button_click)
@@ -299,5 +306,33 @@ add_button.place(x=400, y=100)
 reorganize_button.place(x=100, y=200)
 query_button.place(x=400, y=200)
 
+# New button to print data in CSV file
+def print_csv_data():
+    """
+    param: None
+    typeParam: None
+    rReturn: None
+    rtype: None
+    """
+    if not FILE_PATH:
+        messagebox.showerror("Error", "No file has been imported!")  # Show error
+        return
+
+    csv_data = import_csv_file()
+    text_widget.config(state=tk.DISABLED)
+    text_widget.config(state=tk.NORMAL)
+    text_widget.delete("1.0", tk.END)
+
+    # Display the CSV data in the Text widget
+    for row in csv_data:
+        text_widget.insert(tk.END, ", ".join(row) + "\n")
+
+    # Disable the Text widget after displaying the data
+    text_widget.config(state=tk.DISABLED)
+    text_widget.place(x=125, y=350)
+
+print_button = tk.Button(app, text="Print Data in CSV File",
+            font=custom_font, fg="white", bg="skyblue3", height=2, width=20, command=print_csv_data)
+print_button.place(x=235, y=610)
 
 app.mainloop()
