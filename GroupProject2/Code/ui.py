86723c6235d1ea39b37e13440e69154598046114
GeorgeME1 <georgeebaugh@gmail.com>
2023-10-27 17:21:18 -0400

    Create ui.py

---
+++ GroupProject2/Code/ui.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+def start_game():
+    # Add code here to start your game
+    pass
+
+def quit_game():
+    root.destroy()
+
+# Create the main window
+root = tk.Tk()
+root.title("Game Menu")
+
+# Create a label for the title
+title_label = tk.Label(root, text="Welcome to My Game", font=("Helvetica", 24))
+title_label.pack(pady=20)
+
+# Create buttons for starting the game and quitting
+start_button = tk.Button(root, text="Start Game", command=start_game)
+quit_button = tk.Button(root, text="Quit", command=quit_game)
+
+start_button.pack()
+quit_button.pack()
+
+# Run the Tkinter main loop
+root.mainloop()
