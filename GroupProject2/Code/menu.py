7fe1355c10a3a3363d51e42cbd30c4a02881438f
GeorgeME1 <georgeebaugh@gmail.com>
2023-10-30 16:41:45 -0400

    update 2
    menu update 2

--- GroupProject2/Code/menu.py
+++ GroupProject2/Code/menu.py
@@ -1,6 +1,7 @@
 import subprocess
 import tkinter as tk
-from tkinter import PhotoImage
+from tkinter import ttk
+from ttkthemes import ThemedStyle
 from PIL import Image, ImageTk
 
 def start_game():
@@ -25,29 +26,49 @@ def update_frame():
         update_frame()
 
 
+
 # Create the main window
 root = tk.Tk()
 root.title("Game Menu")
 root.geometry("600x400")
+root.config(bg="#000000")
+
+#style object
+style=ttk.Style()
+theme=ThemedStyle(root)
+
+#confige style
+theme.set_theme("plastik")
+style.configure("TButton",
+                foreground="orange",
+                background="black",
+                padding=(10,5),
+                font=("Helvetica",12),
+                borderwidth=2,
+                relief="raised")
+style.configure("TLabel",
+                foreground="#ff6600",  
+                background="black", 
+                padding=(10, 5))     
 
 #make gif work
-animation=Image.open("GroupProject2/Code/2218246332.gif")
+animation=Image.open("GroupProject2/Code/1062854907.gif")
 img=ImageTk.PhotoImage(animation)
 animation_speed=100
 
 #label for gif
-gif_label=tk.Label(root, image=img)
+gif_label=ttk.Label(root, image=img)
 
 
 
 
 # Create a label for the title
-title_label = tk.Label(root, text="Star Defender", font=("Helvetica", 24))
+title_label = ttk.Label(root, text="Star Defender", font=("Helvetica", 24))
 title_label.pack(pady=20)
 gif_label.pack()
 # Create buttons for starting the game and quitting
-start_button = tk.Button(root, text="Start Game", command=start_game)
-quit_button = tk.Button(root, text="Quit", command=quit_game)
+start_button = ttk.Button(root, text="Start Game", command=start_game)
+quit_button = ttk.Button(root, text="Quit", command=quit_game)
 
 start_button.pack()
 quit_button.pack()
