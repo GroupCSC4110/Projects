681b50b0468603df2f41d3a6fa500abf87aafe5c
GeorgeME1 <georgeebaugh@gmail.com>
2023-11-19 20:34:26 -0500

    progress on menu
    what i have on the menu so far

---
+++ GroupProject3/code/WorkinProgressMenu.py
@@ -0,0 +1,86 @@
+import tkinter as tk
+from tkinter import PhotoImage,ttk
+
+def Graphic_desgin():
+    root.destroy()
+    new_window = tk.Tk()
+    new_window.title("New Window")
+    new_window.geometry("400x300")
+    label = tk.Label(new_window, text="This is a new window!")
+    label.pack()
+    new_window.mainloop()
+
+
+def Human_resources():
+    root.destroy()
+    new_window = tk.Tk()
+    new_window.title("New Window")
+    new_window.geometry("400x300")
+    label = tk.Label(new_window, text="This is a new window!")
+    label.pack()
+    new_window.mainloop()
+
+
+def janitorial():
+    root.destroy()
+    new_window = tk.Tk()
+    new_window.title("New Window")
+    new_window.geometry("400x300")
+    label = tk.Label(new_window, text="This is a new window!")
+    label.pack()
+    new_window.mainloop()
+
+def Software_dev():
+    root.destroy()
+    new_window = tk.Tk()
+    new_window.title("New Window")
+    new_window.geometry("400x300")
+    label = tk.Label(new_window, text="This is a new window!")
+    label.pack()
+    new_window.mainloop()
+
+root = tk.Tk()
+root.title("Main Window")
+root.geometry("800x800")
+
+#Style thing
+style=ttk.Style()
+style.theme_use("clam")
+style.configure("TButton", padding=6, relief="flat", background="#FFFDD0")
+style.configure("TLabel", padding=6, background="#F0F0F0")
+style.configure("TFrame", padding=6, background="#FFFDD0")
+style.configure("TEntry", padding=6)
+root.configure(bg="#0089BB")
+
+# Load the image
+image_1 = PhotoImage(file="GroupProject3\code\GD_new.png")
+image_2 = PhotoImage(file="GroupProject3\code\SD_new.png")
+image_3 = PhotoImage(file="GroupProject3\code\J_new.png")
+image_4 = PhotoImage(file="GroupProject3\code\HR_new.png")
+
+#labels for images
+label_GD=tk.Label(root, )
+
+
+# Create a button with the loaded image for graphic design
+button_GD= ttk.Button(root, image=image_1, command=Graphic_desgin,text="Graphic Design",compound=tk.TOP)
+
+
+# Create a button with the loaded image
+button_SD= ttk.Button(root, image=image_2, command=Software_dev,text="Software developers",compound=tk.TOP)
+
+
+# Create a button with the loaded image
+button_J= ttk.Button(root, image=image_3, command=janitorial,text="Janitorial",compound=tk.TOP)
+
+
+# Create a button with the loaded image
+button_HR= ttk.Button(root, image=image_4, command=Human_resources,text="Human resources",compound=tk.TOP)
+
+
+button_GD.grid(row=0, column=0, padx=10, pady=10)
+button_SD.grid(row=0, column=1, padx=10, pady=10)
+button_J.grid(row=0, column=2, padx=10, pady=10)
+button_HR.grid(row=1, column=0, padx=10, pady=10)
+
+root.mainloop()
